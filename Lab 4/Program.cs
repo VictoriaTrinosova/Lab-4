ab65af6892a93be8544d2b28433c835574c286d6
Виктория <@LAPTOP-CFFC9F7Q>
2021-11-20 15:08:27 +0300

    Добавьте файлы проекта.

---
+++ Lab 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1, c=0; i<=a; i++)
+            {
+                Console.WriteLine("Квадрат числа {0} равен {1}",i,c+=i*2-1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
